9c7baaac89a869eb0d12da1fa95bee526e0fd12b
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-10-21 00:01:11 +0200

    ????

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+        alias: {
+            'blockly': 'blockly/core',
+        }
+    },
 	optimizeDeps: {
 		include: [
 			'@blockly/continuous-toolbox',
