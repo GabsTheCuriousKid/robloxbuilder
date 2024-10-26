be72191b05c9d7535b16bb3b31a9642619fa6a1c
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-10-26 21:36:49 +0200

    time to slowly readd features to know exactly what's wrong

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,35 @@
 <script>
-  import { onMount } from 'svelte';
-  onMount(() => {
-    console.log("Basic component loaded");
-  });
+    import { onMount } from 'svelte';
+
+    // Components
+    import NavigationBar from "$lib/NavigationBar/NavigationBar.svelte";
+    import NavigationDivider from "$lib/NavigationBar/Divider.svelte";
+    import NavigationButton from "$lib/NavigationBar/Button.svelte";
+    import StyledButton from "$lib/StyledComponents/ToolboxButton.svelte";
+
+    onMount(() => {
+        console.log("ignore the warnings above we dont care about those");
+
+        window.onbeforeunload = () => "";
+    });
+
+    function discordInvite() {
+        window.open("https://discord.gg/eVQdK8csJc")
+    }
 </script>
 
-<h1>Hello, SvelteKit! Test to see what may be causing the issue</h1>
+<NavigationBar>
+    <NavigationButton on:click={discordInvite}>Discord</NavigationButton>
+    <NavigationDivider />
+    <NavigationButton on:click={}>Save</NavigationButton>
+    <NavigationButton on:click={}>Load</NavigationButton>
+    <NavigationDivider />
+    <input
+        class="project-name"
+        type="text"
+        placeholder="Script Name (ex: Script)"
+        style="margin-left:4px;margin-right:4px"
+        bind:value={projectName}
+        on:change={updateGeneratedCode}
+    />
+</NavigationBar>
