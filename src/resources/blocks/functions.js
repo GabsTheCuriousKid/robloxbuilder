c97b4b89ebb535ec8dea827cca3735264f99963b
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-10-27 01:56:36 +0200

    fix

--- src/resources/blocks/functions.js
+++ src/resources/blocks/functions.js
@@ -38,7 +38,7 @@ function register() {
         const VALUES = block.getFieldValue('VALUES') || ''
         const FUNC = luaGenerator.statementToCode(block, 'FUNC');
         
-        const code = `function ${ID}(${VALUES}) { ${FUNC} }`;
+        const code = `function ${ID}(${VALUES}) \n${FUNC} end)`;
         return `${code}\n`;
     });
 
@@ -74,8 +74,7 @@ function register() {
         const ID = block.getFieldValue('ID')
         const VALUES = block.getFieldValue('VALUES') || ''
         const FUNC = luaGenerator.statementToCode(block, 'FUNC');
-        
-        const code = `local function ${ID}(${VALUES}) ${FUNC} \nend)`;
+        const code = `local function ${ID}(${VALUES}) \n${FUNC} end)`;
         return `${code}\n`;
     });
 
